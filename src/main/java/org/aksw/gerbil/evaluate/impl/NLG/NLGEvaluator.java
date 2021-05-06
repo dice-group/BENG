9fcd422d1dcecd42ab7a7a0d7079a866ff6b96fe
TortugaAttack <felixconrads@googlemail.com>
2021-05-06 15:29:24 +0200

    p.waitFor hangs fixed, added fallback if still hangs

--- src/main/java/org/aksw/gerbil/evaluate/impl/NLG/NLGEvaluator.java
+++ src/main/java/org/aksw/gerbil/evaluate/impl/NLG/NLGEvaluator.java
@@ -14,6 +14,38 @@ import org.apache.commons.io.IOUtils;
 
 public class NLGEvaluator implements Evaluator<SimpleFileRef> {
 
+    public static void main(String[] args) throws IOException, InterruptedException {
+        ReaderThread reader = new ReaderThread();
+        Thread readerThread = new Thread(reader);
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        processBuilder.command(new String[]{"./test.sh"});
+        Process p = processBuilder.start();
+        reader.setInput(p.getInputStream());
+
+
+        reader.setProcess(p);
+        readerThread.start();
+
+        // Wait for the python process to terminate
+        int exitValue = p.waitFor();
+        // stop the reader thread
+        reader.setTerminate(true);
+        // Wait for the reader thread to terminate
+        readerThread.join();
+
+        // The script encountered an issue
+        if (exitValue != 0) {
+            // Try to get the error message of the script
+            IOUtils.copy(p.getErrorStream(), System.err);
+            throw new IllegalStateException("Python script aborted with an error.");
+        }
+
+        String scriptResult = reader.getBuffer().toString();
+        System.out.println("Data:" + scriptResult + "\n");
+
+
+    }
+
     @Override
     public void evaluate(List<List<SimpleFileRef>> annotatorResults, List<List<SimpleFileRef>> goldStandard,
                          EvaluationResultContainer results, String language) {
@@ -28,23 +60,25 @@ public class NLGEvaluator implements Evaluator<SimpleFileRef> {
         System.out.println("Language: "+ language);
         // start python script and gather results
 
-            String command = "";
+            String[] command;
             ReaderThread reader = new ReaderThread();
             Thread readerThread = new Thread(reader);
             if (language.equals("ru")){
-                command = new StringBuilder().append("python3 src/main/java/org/aksw/gerbil/python/mt/eval.py -R ")
-                       .append(ref).append(" -H ").append(hypo).append(" -lng ru -nr 1")
-                       .append(" -m bleu,meteor,chrf++,ter").toString();
-               System.out.println("command: "+ command);
-
+                command = new String[]{"python3", "src/main/java/org/aksw/gerbil/python/mt/eval.py",
+                        "-R", ref.getAbsolutePath(), "-H",hypo.getAbsolutePath(), "-lng", "ru", "-nr", "1",
+                        "-m", "bleu,meteor,chrf++,ter"};
             }else{
-                command = new StringBuilder().append("python3 src/main/java/org/aksw/gerbil/python/mt/eval.py -R ")
-                        .append(ref).append(" -H ").append(hypo).append(" -nr 1")
-                        .append(" -m bleu,meteor,chrf++,ter").toString();
-                System.out.println("command: "+ command);
+                command = new String[]{"python3", "src/main/java/org/aksw/gerbil/python/mt/eval.py",
+                        "-R", ref.getAbsolutePath(), "-H",hypo.getAbsolutePath(), "-nr", "1",
+                        "-m", "bleu,meteor,chrf++,ter"};
            }
+        System.out.println("command: "+ command);
+
         try {
-            Process p = Runtime.getRuntime().exec(command);
+
+            ProcessBuilder processBuilder = new ProcessBuilder();
+            processBuilder.command(command);
+            Process p = processBuilder.start();
             reader.setInput(p.getInputStream());
             readerThread.start();
 
@@ -94,6 +128,7 @@ public class NLGEvaluator implements Evaluator<SimpleFileRef> {
 
         private StringBuilder buffer = new StringBuilder();
         private Reader input;
+        private Process process;
         private boolean terminate = false;
 
         @Override
@@ -101,19 +136,28 @@ public class NLGEvaluator implements Evaluator<SimpleFileRef> {
             try {
                 char cBuffer[] = new char[256];
                 int length;
-                while (!terminate) {
-                    while ((input != null) && (input.ready())) {
-                        length = input.read(cBuffer);
+                while(process.isAlive()) {
+                    while ((length = input.read(cBuffer)) != -1) {
                         buffer.append(cBuffer, 0, length);
                     }
                     // sleep for a short moment before checking the stream again
                     Thread.sleep(50);
+                }
+                Thread.sleep(50);
+                //process is not alive, but terminate should not be set to true, if not then p.waitFor hangs for some reason.
+                //This is just a fallback
+                if(!terminate){
+                    process.destroyForcibly();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+        }
+
+        public void setProcess(Process p){
+            this.process = p;
         }
 
         public void setInput(InputStream input) {
